6a64e05274f0dafcf3d43290924074a92246452c
Markus Staab <markus.staab@redaxo.de>
2017-03-18 13:59:20 +0100

    disable settings which dont make sense in rex context
    as we already inherit this things from the rex-backend context

--- pages/index.php
+++ pages/index.php
@@ -22,6 +22,18 @@ ob_start(function ($output) {
 // declare all globals required by Simple-Stats
 global $ss, $filters, $page, $ajax, $script_i18n, $is_archive, $loaded_data, $date_label, $has_filters, $field_names, $ua;
 
+echo '<style>
+#options-form tr:nth-child(2),/* site-name */
+#options-form tr:nth-child(3),/* require-login-to-edit-config*/
+#options-form tr:nth-child(4),/* require-login-to-view-stats*/
+#options-form tr:nth-child(5),/* require-login: username*/
+#options-form tr:nth-child(6),/* require-login: passwords*/
+#options-form tr:nth-child(8) /* language */
+{
+	display: none;
+}
+</style>';
+
 require_once( $this->getPath('vendor/Simple-Stats/index.php') );
 
 // make sure the output buffer callback is called
